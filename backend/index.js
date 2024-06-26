b7612ec9a992c9b6840f06f79a4e9d7b914aa9e1
gaurav <gauravkumarkarki07@gmail.com>
2024-06-26 23:55:00 +1000

    deploy

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ import TenantRouter from './routes/TenantRouter.js';
 import LandlordRouter from './routes/LandlordRouter.js';
 import PropertyRouter from './routes/PropertyRouter.js';
 import { ErrorHandler } from './utils/ErrorHandler.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config()
 
@@ -29,7 +31,18 @@ app.use('/ghar/tenant',TenantRouter);
 app.use('/ghar/landlord',LandlordRouter);
 app.use('/ghar/property',PropertyRouter);
 
+// Determine __dirname equivalent in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Serve static files in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist"))); 
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html")); 
+    });
+}
 
 //Undefined Routes
 app.use('*',(req,res,next)=>{
