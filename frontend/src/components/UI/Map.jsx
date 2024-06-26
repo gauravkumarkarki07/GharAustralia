c30d33e50dccf67c13fed3cd5acbd698a3450617
gaurav <gauravkumarkarki07@gmail.com>
2024-06-27 00:42:06 +1000

    Ready

--- frontend/src/components/UI/Map.jsx
+++ frontend/src/components/UI/Map.jsx
@@ -3,34 +3,14 @@ import "leaflet/dist/leaflet.css";
 import MapMarker from "./MapMarker";
 import PropTypes from 'prop-types';
 
-Map.propTypes={
-  location:PropTypes.object,
-  data:PropTypes.any
-}
-
+Map.propTypes = {
+  location: PropTypes.object,
+  data: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+};
 
 export default function Map({ location, data }) {
   const isArray = Array.isArray(data);
   const zoomLevel = isArray ? 10 : 20;
-
-  const renderMarkers = () => {
-    if (isArray) {
-      return data.map((property, index) => (
-        <MapMarker 
-          key={index} 
-          geoLocation={{ latitude: property.latitude, longitude: property.longitude }} 
-          details={property} 
-        />
-      ));
-    } else {
-      return (
-        <MapMarker 
-          geoLocation={{ latitude: data.latitude, longitude: data.longitude }} 
-          details={data} 
-        />
-      );
-    }
-  };
 
   return (
     <div id="map" className="w-full h-full">
@@ -44,7 +24,20 @@ export default function Map({ location, data }) {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {renderMarkers()}
+        {isArray ? (
+          data.map((property, index) => (
+            <MapMarker 
+              key={index} 
+              geoLocation={{ latitude: property.latitude, longitude: property.longitude }} 
+              details={property} 
+            />
+          ))
+        ) : (
+          <MapMarker 
+            geoLocation={{ latitude: data.latitude, longitude: data.longitude }} 
+            details={data} 
+          />
+        )}
       </MapContainer> 
     </div>
   );
